e3d22cadeec446443a2cfcff26d7944940431f1a
yashjsalian23 <yashjsalian23@gmail.com>
2020-05-22 19:51:08 +0700

    intialized passport

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy ;
 const app = express();
 
+passport.use(new googleStrategy());
+
 app.get("/", (req, res) => {
-    res.send({message: "Hello World"});
+    res.send({message: "Hello World updated"});
 });
 
 const PORT = process.env.PORT || 5000;
