517dd118e545cfc5376ac50fc25c73eaf6d822ac
yashjsalian23 <yashjsalian23@gmail.com>
2020-05-22 21:33:02 +0700

    intialized oauth

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy ;
+const keys = require('./config/keys');
+
 const app = express();
 
-passport.use(new googleStrategy());
+passport.use(new googleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, (accessToken,refreshToken,profile,done) => {
+        console.log("accessToken",accessToken);
+        console.log("refreshToken",refreshToken);
+        console.log("profile",profile);
+    })
+);
 
 app.get("/", (req, res) => {
     res.send({message: "Hello World updated"});
 });
+
+app.get("/auth/google", passport.authenticate('google',{
+    scope: ['profile', 'email']
+}));
+
+app.get("/auth/google/callback", passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
