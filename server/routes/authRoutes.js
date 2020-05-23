93431ed7d3a19a49b6dcf4230936beb9a7f908c2
yashjsalian23 <yashjsalian23@gmail.com>
2020-05-23 19:24:36 +0700

    completed basic auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,4 +10,13 @@ module.exports = (app) => {
     app.get("/api/current_user", (req, res) => {
         res.send(req.user);
     });
+
+    app.get("/api/logout", (req,res) => {
+        req.logout();
+        res.send({message: "Sucessfully Logged Out!"});
+        // res.send(req.user);
+        
+    })
+
+
 }  
