b047e37209351162a479debbec28cf3dc188f315
yashjsalian23 <yashjsalian23@gmail.com>
2020-05-23 19:24:05 +0700

    completed basic auth

--- server/index.js
+++ server/index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/Users');
 require('./services/passport'); //make sure passport is below the users schema
 
 const app = express();
 
+app.use(cookieSession({
+  maxAge : 2592000000,
+  keys : [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
     res.send({message: "Hello World updated"});
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,14 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => done(null, user));
+});
 
 passport.use(new googleStrategy({
     clientID: keys.googleClientID,
