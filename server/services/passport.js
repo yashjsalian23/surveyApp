9087a225c828323d5025274bc30b228f7ed7dd31
yashjsalian23 <yashjsalian23@gmail.com>
2020-05-26 20:57:45 +0700

    commit

--- server/services/passport.js
+++ server/services/passport.js
@@ -19,16 +19,13 @@ passport.use(new googleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: "/auth/google/callback",
     proxy: true
-}, (accessToken,refreshToken,profile,done) => {
-    User.findOne({ googleId: profile.id})
-    .then(existingUser => {
+}, async (accessToken,refreshToken,profile,done) => {
+    const existingUser = await User.findOne({ googleId: profile.id})
         if(existingUser){
-            done(null, existingUser); //first argument is the error one. If there is an error we need to pass it, since we dont have any error here we pass null
-        } else {
-            new User({ googleId: profile.id}).save()
-            .then(user => done(null,user));
-        }
-    })
+            return done(null, existingUser); //first argument is the error one. If there is an error we need to pass it, since we dont have any error here we pass null
+        } 
+            const user = await new User({ googleId: profile.id}).save()
+            done(null,user);
 })
 );
 
