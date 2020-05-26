608ff276af9cac4557671c410cffbd15aec71e26
yashjsalian23 <yashjsalian23@gmail.com>
2020-05-26 21:44:59 +0700

    redux setup complete

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reducers from './reducers';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// registerServiceWorker();
