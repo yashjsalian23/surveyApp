fed89b8570bfa21901352eef4c2baeee93e8b507
yashjsalian23 <yashjsalian23@gmail.com>
2020-05-29 16:13:22 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -28,6 +28,15 @@ app.get("/", (req, res) => {
 authRoutes(app);
 billingRoutes(app);
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+};
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(keys.mongoURI)
