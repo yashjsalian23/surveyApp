9404892eb6423f5ee0e08b25991cb37ab9dadb36
yashjsalian23 <yashjsalian23@gmail.com>
2020-05-30 17:13:40 +0700

    survey request handler

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,8 +1,18 @@
+const Survey = require('../models/Surveys');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredit');
 
+
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
-
+        const { title, subject, body, recipients} = req.body;
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients:  recipients.split(',').map(email => ({ email: email.trim() })), 
+            _user: req.user.id,
+            dateSent: Date.now()
+        })
     });
 }
