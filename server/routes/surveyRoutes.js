f78fba04e88b1187832b3c3410f0a0c4232b9433
yashjsalian23 <yashjsalian23@gmail.com>
2020-06-01 12:28:41 +0700

    completed Mailer class and its confirguations

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const Survey = require('../models/Surveys');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredit');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 
 module.exports = app => {
@@ -13,6 +15,8 @@ module.exports = app => {
             recipients:  recipients.split(',').map(email => ({ email: email.trim() })), 
             _user: req.user.id,
             dateSent: Date.now()
-        })
+        });
+
+        const mailer = new Mailer(survey, surveyTemplate(survey));
     });
 };
