33df07f4825a5486814b5ab4669b7b33a9d66a92
yashjsalian23 <yashjsalian23@gmail.com>
2020-06-01 20:43:49 +0700

    survey backend completed

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -7,6 +7,11 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+    app.get("/api/surveys/thanks", (req, res) => {
+        res.send("THANKS FOR YOUR FEEDBACK");
+    });
+
+
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients} = req.body;
         const survey = new Survey({
@@ -19,6 +24,14 @@ module.exports = app => {
         });
 
         const mailer = new Mailer(survey, surveyTemplate(survey));
-        mailer.send();
+        try{
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 };
