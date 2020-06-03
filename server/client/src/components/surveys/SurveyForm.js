178acc1bd1f9f9e7747383ebb69ac8b959d99ab0
yashjsalian23 <yashjsalian23@gmail.com>
2020-06-04 01:28:15 +0700

    completed email validation

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
-    {key: "1", label: 'Survey Title', name: 'title'},
-    {key: "2", label: 'Survey Line', name: 'subject'},
-    {key: "3", label: 'Email Body', name: 'body'},
-    {key: "4", label: 'Recipient List', name: 'emails'}      
+    {key: "1", label: 'Survey Title', name: 'title', placeholder: 'Title'},
+    {key: "2", label: 'Survey Line', name: 'subject', placeholder: 'Subject'},
+    {key: "3", label: 'Email Body', name: 'body', placeholder: 'Body'},
+    {key: "4", label: 'Recipient List (seprate by ,)', name: 'emails', placeholder: 'Emails separated by commas(,)'}      
 ];
 
 class SurveyForm extends Component{
     renderFields() {
-        return FIELDS.map(({label, name, key}) => {
-            return <Field key={key} component={SurveyField} type="text" label={label} name={name} />
+        return FIELDS.map(({label, name, key, placeholder}) => {
+            return <Field key={key} placeholder={placeholder} component={SurveyField} type="text" label={label} name={name} />
         });
     };
     
@@ -37,11 +38,14 @@ class SurveyForm extends Component{
 
 function validate(values){
     const errors = {};
+
+    errors.emails = validateEmails(values.emails || ' ');
     FIELDS.forEach(({name}) => {
         if(!values[name]){
-            errors[name] = `You must provide a ${name}`;
+            errors[name] = `You must provide ${name}`;
         }
     })
+
     return errors;
 }
 
