c5f604567242f2bbbc1a90303e073d0bd20af3f4
yashjsalian23 <yashjsalian23@gmail.com>
2020-06-08 14:18:21 +0700

    completed webhooks and survey update

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,6 @@
 // const Survey = require('../models/Surveys');
 const _ = require('lodash');
-const Path = require('path-parser');
+const { Path } = require('path-parser');
 const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
@@ -15,7 +15,31 @@ module.exports = app => {
     });
 
     app.post("/api/surveys/webhooks", (req, res) => {
-        console.log(req.body);
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+         _.chain(req.body)
+         .map(({email, url}) => {
+            const match = p.test(new URL(url).pathname);
+            if(match){
+                return { email, surveyId: match.surveyId, choice: match.choice}
+            }
+        })
+         .compact()
+         .uniqBy('email', 'surveyId')
+         .each(({surveyId, email, choice}) => {
+            Survey.updateOne({
+                _id: surveyId,
+                recipients: {$elemMatch: { email: email, responded: false }}
+            },
+            {
+                $inc: { [choice]: 1 },
+                $set: { 'recipients.$.responded': true },
+                lastResponded: new Date()
+            }).exec();
+         })
+         .value();
+
+        
         res.send({});
     });
 
