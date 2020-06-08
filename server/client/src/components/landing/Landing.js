504aa63f8c5975d1721b76ac7b936cfd43444944
yashjsalian23 <yashjsalian23@gmail.com>
2020-06-08 19:23:07 +0700

    landing change

--- server/client/src/components/landing/Landing.js
+++ server/client/src/components/landing/Landing.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Landing = () => {
-    return (
-        <div>
-            <h1>Landing</h1>
-        </div>
-    )
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Emaily!
+      </h1>
+      Collect feedback form your users
+    </div>
+  );
 };
 
 export default Landing;
